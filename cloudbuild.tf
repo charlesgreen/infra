a10e79e7938f837bcabe75d36507daebbfd6c1ef
Charles Green <cg@simplycubed.com>
2022-02-20 17:48:32 +0900

    Update cloudbuild config

--- cloudbuild.tf
+++ cloudbuild.tf
@@ -84,7 +84,7 @@ resource "google_cloudbuild_trigger" "deploy_web" {
     _FIREBASE_MESSAGING_SENDER_ID = var.firebase_messaging_sender_id
     _FIREBASE_STORAGE_BUCKET      = var.firebase_storage_bucket
   }
-  filename = "cloudbuild.main.yaml"
+  filename = var.env == "prod" ? "cloudbuild.prod.yaml" : "cloudbuild.dev.yaml"
   tags     = ["managed by terraform"]
 }
 
